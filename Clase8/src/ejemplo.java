9e7bdb693d5ce31cdc3f64001138afeaae903687
Lubswer <lubswer@gmail.com>
2025-11-14 12:42:18 -0500

    Eercicio Clase

--- Clase8/src/ejemplo.java
+++ Clase8/src/ejemplo.java
@@ -14,22 +14,24 @@ public class ejemplo {
             sc.nextLine();
             switch (opcion) {
                 case 1:
-                    try{
+                    try {
                         System.out.print("Ingrese su nombre: ");
                         nombre = sc.nextLine();
-                        if(nombre == null || nombre.isBlank()){
+                        if (nombre == null || nombre.isBlank()) {
                             throw new IllegalArgumentException("Error de String");
 
-                        }else{
+                        } else {
                             System.out.println("Tu nombre es: " + nombre);
                         }
-                        sc.nextLine();
 
-                    }catch (IllegalArgumentException e){
-                        System.out.println("Tipo de dato nulo o vacio");
+
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("Ups! salio algo mal ----> Tipo de dato nulo o vacio");
+                        System.out.println("Porque ---->  " + e.getMessage());
                     }
 
                     break;
+
                 case 2:
                     try {
                         System.out.print("Ingrese el primer numero: ");
@@ -42,17 +44,19 @@ public class ejemplo {
                         if (numero2 < 0) {
                             throw new ArithmeticException();
                         }
-                        if(numero1 > 0 &&  numero2 > 0){
+                        if (numero1 > 0 && numero2 > 0) {
                             System.out.println("El primer numero es: " + numero1);
                             System.out.println("El segundo numero es: " + numero2);
                             System.out.println("Tus numeros sumados: " + (numero1 + numero2));
                         }
                     } catch (ArithmeticException e) {
-                        System.out.println("Los no pueden ser menores a cero");
+                        System.out.println("Ups! salio algo mal ---->  Los no pueden ser menores a cero");
+                        System.out.println("Porque ---->  " + e.getMessage());
 
                     } catch (Exception e) {
-                        System.out.println("El tipo de dato ingresado no es un numero entero");
-                    }finally {
+                        System.out.println("Ups! salio algo mal ---->  El tipo de dato ingresado no es un numero entero");
+                        System.out.println("Porque ---->  " + e.getMessage());
+                    } finally {
                         System.out.println("Fin del ingreso de numeros");
                     }
                     break;
@@ -60,11 +64,8 @@ public class ejemplo {
                     System.out.println("Saliendo...");
                     break;
                 default:
-                    System.out.println("Opcion no valida");
+                    System.out.println(("Opcion incorrecta"));
             }
-        }while(opcion != 3);
-
-
-
+        } while (opcion != 3);
     }
 }
